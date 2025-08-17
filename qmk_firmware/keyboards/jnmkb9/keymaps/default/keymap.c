f2a5b759ddde2f78ea1dd62573fb0e15ebd6e140
gipsohlavi <gipsohlavi@gmail.com>
2025-08-17 23:30:18 +0900

    no message

---
+++ qmk_firmware/keyboards/jnmkb9/keymaps/default/keymap.c
@@ -0,0 +1,21 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┐
+     * │ 1 │ 2 │ 3 │
+     * ├───┼───┼───┤
+     * │ 4 │ 5 │ 6 │
+     * ├───┼───┼───┤
+     * │ 7 │ 8 │ 9 │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_KP_1,KC_KP_2,KC_KP_3,
+        KC_KP_4,KC_KP_5,KC_KP_6,
+        KC_KP_7,KC_KP_8,KC_KP_9
+    )
+};
